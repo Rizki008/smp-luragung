cd2f80acb47c2bf4107cabd9581ae05c3dd75c45
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-31 08:47:16 +0700

    view utama frontend

---
+++ application/views/v_home.php
@@ -0,0 +1,106 @@
+<div class="hero-slide owl-carousel site-blocks-cover">
+	<div class="intro-section" style="background-image: url('<?= base_url('frontend') ?>/images/hero_1.jpg');">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-12 mx-auto text-center" data-aos="fade-up">
+					<h1>SMP 2 Luragung</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="intro-section" style="background-image: url('<?= base_url('frontend/') ?>images/hero_1.jpg');">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-12 mx-auto text-center" data-aos="fade-up">
+					<h1>Profil Tentang Kami</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+<div></div>
+
+<div class="site-section">
+	<div class="container">
+		<div class="row mb-5 justify-content-center text-center">
+			<div class="col-lg-4 mb-5">
+				<h2 class="section-title-underline mb-5">
+					<span>Tentang SMP 2 Luragung</span>
+				</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+
+				<div class="feature-1 border">
+					<div class="icon-wrapper bg-primary">
+						<span class="flaticon-mortarboard text-white"></span>
+					</div>
+					<div class="feature-1-content">
+						<h2>Ruang Laboratorium</h2>
+						<p>Tedapat 2 ruangan laboratorium, lab. komputer dan lab. ipa</p>
+						<!-- <p><a href="#" class="btn btn-primary px-4 rounded-0">Learn More</a></p> -->
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+				<div class="feature-1 border">
+					<div class="icon-wrapper bg-primary">
+						<span class="flaticon-school-material text-white"></span>
+					</div>
+					<div class="feature-1-content">
+						<h2>Ruang Kelas</h2>
+						<p>Terdapat 12 ruangan kelas dari kelas VII sampe kelas IX</p>
+						<!-- <p><a href="#" class="btn btn-primary px-4 rounded-0">Learn More</a></p> -->
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+				<div class="feature-1 border">
+					<div class="icon-wrapper bg-primary">
+						<span class="flaticon-library text-white"></span>
+					</div>
+					<div class="feature-1-content">
+						<h2>Ruang Perpustakaan</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+						<!-- <p><a href="#" class="btn btn-primary px-4 rounded-0">Learn More</a></p> -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+<div class="section-bg style-1" style="background-image: url('<?= base_url() ?>frontend/images/about_1.jpg');">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<h2 class="section-title-underline style-2">
+					<span>Tentang SMP 2 Luragung</span>
+				</h2>
+			</div>
+			<div class="col-lg-8">
+				<h5 class="lead">Visi</h5>
+				<p>Terwujudnya Sekolah Sehat, Berprestasi dan Berakhlaq Mulia Tahun 2020</p>
+				<h5 class="lead">Misi</h5>
+				<p> 1. Meningkatkan Iman Dan Taqwa Terhadap Tuhan Yang Maha Esa;</p>
+				<p> 2. Meningkatkan Disiplin Seluruh Warga Sekolah;</p>
+				<p> 3. Meningkatkan Professionalisme Tenaga Pendidik Dan Kependidikan;</p>
+				<p>4. Melaksanakan Pembelajaran Yang Aktif, Inovatif, Kreatif, Efektif, dan Menyenangkan;<br>
+					5. Mempersiapkan Siswa Mengikuti Berbagai Lomba Akademik dan Non Akademik;<br>
+					6. Mengembangkan Ketrampilan Menulis Dengan Memanfaatkan IT;<br>
+					7. Menumbuhkan Minat Baca (Budaya Literasi);<br>
+					8. Mengedepankan Kebersamaan dalam Mewujudkan Kesejahteraan Warga Sekolah;<br>
+					9. Melestarikan Lingkungan Sekolah yang Sehat dan Berbudaya Lingkungan;
+				</p>
+			</div>
+		</div>
+	</div>
+</div>

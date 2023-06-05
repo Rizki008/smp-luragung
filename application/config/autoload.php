26dd1842d1a7385b515eb4bb8623f7f9001b4734
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-06 00:57:51 +0700

    menambah library login user

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'database', 'upload', 'siswa_login');
+$autoload['libraries'] = array('session', 'form_validation', 'database', 'upload', 'siswa_login', 'admin_login');
 
 /*
 | -------------------------------------------------------------------

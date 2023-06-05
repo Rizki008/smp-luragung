877f33b36320068c082e58c7e0a014d9044859de
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-06 00:55:02 +0700

    kelola chatting

---
+++ application/controllers/Chatting.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Chatting extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_chatting');
+	}
+	public function index()
+	{
+
+		$this->form_validation->set_rules('message', 'Pesan', 'required');
+
+		if ($this->form_validation->run() == FALSE) {
+			// $this->pelanggan_login->proteksi_halaman();
+			$data = array(
+				'title' => 'Chatting',
+				'chat' => $this->m_chatting->chat(),
+				'isi' => 'frontend/chatting/v_chatting'
+			);
+			$this->load->view('frontend/v_wrapper', $data);
+		} else {
+			$data = array(
+				'id_siswa' => $this->input->post('id_siswa'),
+				// 'id_user' => '1',
+				'siswa_send' => $this->input->post('message')
+			);
+			$this->m_chatting->siswa_send($data);
+			redirect('chatting');
+		}
+	}
+}
+
+/* End of file cChatting.php */

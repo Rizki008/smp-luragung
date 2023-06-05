2646bda81003455a1de74824e32ea30395775b6d
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-06 00:58:03 +0700

    kelola crud user

--- application/models/M_auth.php
+++ application/models/M_auth.php
@@ -31,4 +31,33 @@ class M_auth extends CI_Model
 		$this->db->where('id_siswa', $this->session->userdata('id_siswa'));
 		return $this->db->get()->result();
 	}
+
+	public function user()
+	{
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->order_by('id_user', 'desc');
+		return $this->db->get()->result();
+	}
+	public function add($data)
+	{
+		$this->db->insert('user', $data);
+	}
+	public function detail_user($id_user)
+	{
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->where('id_user', $id_user);
+		return $this->db->get()->row();
+	}
+	public function update($data)
+	{
+		$this->db->where('id_user', $data['id_user']);
+		$this->db->update('user', $data);
+	}
+	public function delete($data)
+	{
+		$this->db->where('id_user', $data['id_user']);
+		$this->db->delete('user', $data);
+	}
 }

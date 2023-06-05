4286b3dd6c435faeda35f0416064d07f73036f1a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-06 00:56:43 +0700

    kelola menu

--- application/views/backend/v_nav.php
+++ application/views/backend/v_nav.php
@@ -8,82 +8,41 @@
 		<a href="#" class="sidebar-toggler flex-shrink-0">
 			<i class="fa fa-bars"></i>
 		</a>
-		<form class="d-none d-md-flex ms-4">
+		<!-- <form class="d-none d-md-flex ms-4">
 			<input class="form-control bg-dark border-0" type="search" placeholder="Search">
-		</form>
+		</form> -->
 		<div class="navbar-nav align-items-center ms-auto">
+			<?php
+			$jml_chatting = $this->m_chatting->jml_chatting();
+			$daftar_chat = $this->m_chatting->daftar_chat();
+			?>
 			<div class="nav-item dropdown">
 				<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
 					<i class="fa fa-envelope me-lg-2"></i>
-					<span class="d-none d-lg-inline-flex">Message</span>
+					<span class="d-none d-lg-inline-flex">Message [<?= $jml_chatting ?>]</span>
 				</a>
 				<div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-					<a href="#" class="dropdown-item">
-						<div class="d-flex align-items-center">
-							<img class="rounded-circle" src="<?= base_url() ?>backend/img/user.jpg" alt="" style="width: 40px; height: 40px;">
-							<div class="ms-2">
-								<h6 class="fw-normal mb-0">Jhon send you a message</h6>
-								<small>15 minutes ago</small>
+					<?php foreach ($daftar_chat as $key => $value) { ?>
+						<a href="<?= base_url('admin/view_chatting/' . $value->id_siswa) ?>" class="dropdown-item">
+							<div class="d-flex align-items-center">
+								<div class="ms-2">
+									<h6 class="fw-normal mb-0"><?= $value->nama_lengkap ?></h6>
+									<small><?= $value->time ?></small>
+								</div>
 							</div>
-						</div>
-					</a>
-					<hr class="dropdown-divider">
-					<a href="#" class="dropdown-item">
-						<div class="d-flex align-items-center">
-							<img class="rounded-circle" src="<?= base_url() ?>backend/img/user.jpg" alt="" style="width: 40px; height: 40px;">
-							<div class="ms-2">
-								<h6 class="fw-normal mb-0">Jhon send you a message</h6>
-								<small>15 minutes ago</small>
-							</div>
-						</div>
-					</a>
-					<hr class="dropdown-divider">
-					<a href="#" class="dropdown-item">
-						<div class="d-flex align-items-center">
-							<img class="rounded-circle" src="<?= base_url() ?>backend/img/user.jpg" alt="" style="width: 40px; height: 40px;">
-							<div class="ms-2">
-								<h6 class="fw-normal mb-0">Jhon send you a message</h6>
-								<small>15 minutes ago</small>
-							</div>
-						</div>
-					</a>
-					<hr class="dropdown-divider">
-					<a href="#" class="dropdown-item text-center">See all message</a>
-				</div>
-			</div>
-			<div class="nav-item dropdown">
-				<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-					<i class="fa fa-bell me-lg-2"></i>
-					<span class="d-none d-lg-inline-flex">Notificatin</span>
-				</a>
-				<div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-					<a href="#" class="dropdown-item">
-						<h6 class="fw-normal mb-0">Profile updated</h6>
-						<small>15 minutes ago</small>
-					</a>
-					<hr class="dropdown-divider">
-					<a href="#" class="dropdown-item">
-						<h6 class="fw-normal mb-0">New user added</h6>
-						<small>15 minutes ago</small>
-					</a>
-					<hr class="dropdown-divider">
-					<a href="#" class="dropdown-item">
-						<h6 class="fw-normal mb-0">Password changed</h6>
-						<small>15 minutes ago</small>
-					</a>
-					<hr class="dropdown-divider">
-					<a href="#" class="dropdown-item text-center">See all notifications</a>
+						</a>
+						<hr class="dropdown-divider">
+					<?php } ?>
+					<!-- <a href="#" class="dropdown-item text-center">See all message</a> -->
 				</div>
 			</div>
 			<div class="nav-item dropdown">
 				<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
 					<img class="rounded-circle me-lg-2" src="<?= base_url() ?>backend/img/user.jpg" alt="" style="width: 40px; height: 40px;">
-					<span class="d-none d-lg-inline-flex">John Doe</span>
+					<span class="d-none d-lg-inline-flex"><?= $this->session->userdata('nama_user') ?></span>
 				</a>
 				<div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-					<a href="#" class="dropdown-item">My Profile</a>
-					<a href="#" class="dropdown-item">Settings</a>
-					<a href="#" class="dropdown-item">Log Out</a>
+					<a href="<?= base_url('admin/logout') ?>" class="dropdown-item">Log Out</a>
 				</div>
 			</div>
 		</div>

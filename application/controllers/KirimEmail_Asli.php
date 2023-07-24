3c85b63cbe0c48fa1b8f0b9b2c7d2ef78373b266
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-24 22:56:59 +0700

    backup

---
+++ application/controllers/KirimEmail_Asli.php
@@ -0,0 +1,89 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class KirimEmail extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		require APPPATH . 'libraries/phpmailer/src/Exception.php';
+		require APPPATH . 'libraries/phpmailer/src/PHPMailer.php';
+		require APPPATH . 'libraries/phpmailer/src/SMTP.php';
+
+		$this->load->model('m_chatting');
+	}
+
+	public function index()
+	{
+		$data = array(
+			'title' => 'Kirim pesan lulus',
+			'isi' => 'form_email'
+		);
+		$this->load->view('backend/v_wrapper', $data);
+	}
+
+
+	public function send()
+	{
+		// PHPMailer object
+		$response = false;
+		$mail = new PHPMailer();
+
+		// SMTP configuration
+		$mail->isSMTP();
+		$mail->Host     = 'smtp.gmail.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = 'kr044401@gmail.com'; // user email anda
+		$mail->Password = 'gvkywvrkatayokwc'; // diisi dengan App Password yang sudah di generate
+		$mail->SMTPSecure = 'ssl';
+		$mail->Port     = 465;
+
+		$mail->setFrom('kr044401@gmail.com', 'SMP 2 Luragung'); // user email anda
+		$mail->addReplyTo('kr044401@gmail.com', ''); //user email anda
+
+		// Email subject
+		$mail->Subject = 'Informasi Penerimaan Siswa | SMP 2 Luragung.com'; //subject email
+
+		// Add a recipient
+		$mail->addAddress($this->input->post('email')); //email tujuan pengiriman email
+
+		// Set email format to HTML
+		$mail->isHTML(true);
+
+		// Email body content
+		$mailContent = "<p>Hallo <b>" . $this->input->post('nama') . "</b> berikut ini adalah informasi terkait pendaftaran Anda:</p>
+		<table>
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td>" . $this->input->post('nama') . "</td>
+			</tr>
+			<tr>
+				<td>Status</td>
+				<td>:</td>
+				<td>" . $this->input->post('status') . "</td>
+			</tr>
+			<tr>
+				<td>Keterangan</td>
+				<td>:</td>
+				<td>" . $this->input->post('keterangan') . "</td>
+			</tr>
+		</table>
+   		<p>Terimakasih <b>" . $this->input->post('nama') . "</b> telah mendaftar sebagai calon siswa baru di SMP 2 Luragung.</p>"; // isi email
+		$mail->Body = $mailContent;
+
+		// Send email
+		if (!$mail->send()) {
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		} else {
+			echo 'Pesan Terkirim';
+			echo "<a href='http://localhost/smp-luragung/penerimaan' class='btn btn-success'>Kembali</a>";
+		}
+	}
+}

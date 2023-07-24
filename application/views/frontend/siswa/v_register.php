4d21bc96dd18110c9474ecb04d074d0a96854548
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-25 00:12:50 +0700

    menghapus data ttl.,alamat,asal sekolah

--- application/views/frontend/siswa/v_register.php
+++ application/views/frontend/siswa/v_register.php
@@ -21,7 +21,7 @@
 <div class="site-section">
 	<div class="container">
 		<div class="row justify-content-center">
-			<div class="col-md-5">
+			<div class="col-md-10">
 				<?php
 				//notifikasi form kosong
 				echo validation_errors('<div class="alert alert-warning alert-st-three alert-st-bg2 alert-st-bg13" role="alert">
@@ -30,61 +30,58 @@
 
 				?>
 				<form method="post" action="<?= base_url('siswa/register') ?>" id="CustomerLoginForm" accept-charset="UTF-8" class="contact-form">
+
 					<div class="row">
-						<div class="col-md-12 form-group">
-							<label for="nama_lengkap">nama_lengkap</label>
+						<div class="col-md-6 form-group">
+							<label for="nama_lengkap">Nama Lengkap</label>
 							<input type="text" id="nama_lengkap" name="nama_lengkap" value="<?= set_value('nama_lengkap') ?>" class=" form-control form-control-lg">
 							<?= form_error('nama_lengkap', '<small class="text-danger">', '</small>') ?>
 						</div>
-						<div class="col-md-12 form-group">
-							<label for="nis">nis</label>
-							<input type="text" id="nis" name="nis" value="<?= set_value('nis') ?>" class=" form-control form-control-lg">
-							<?= form_error('nis', '<small class="text-danger">', '</small>') ?>
-						</div>
-						<div class="col-md-12 form-group">
-							<label for="ttl">Tempat, Tanggal Lahir</label>
-							<input type="text" id="ttl" name="ttl" value="<?= set_value('ttl') ?>" class=" form-control form-control-lg">
-							<?= form_error('ttl', '<small class="text-danger">', '</small>') ?>
-						</div>
-						<div class="col-md-12 form-group">
+						<div class="col-md-6 form-group">
 							<label for="jk">Jenis Kelamin</label>
-							<input type="text" id="jk" name="jk" value="<?= set_value('jk') ?>" class=" form-control form-control-lg">
+							<select name="jk" class="form-control" id="jk">
+								<option>---PILIH JENIS KELAMIN---</option>
+								<option value="1">Laki-Laki</option>
+								<option value="2">Perempuan</option>
+							</select>
+							<!-- <input type="text" id="jk" name="jk" value="<?= set_value('jk') ?>" class=" form-control form-control-lg"> -->
 							<?= form_error('jk', '<small class="text-danger">', '</small>') ?>
 						</div>
-						<div class="col-md-12 form-group">
+					</div>
+					<div class="row">
+						<div class="col-md-6 form-group">
 							<label for="no_hp_siswa">Nomor Hp Siswa</label>
-							<input type="text" id="no_hp_siswa" name="no_hp_siswa" value="<?= set_value('no_hp_siswa') ?>" class=" form-control form-control-lg">
+							<input type="number" id="no_hp_siswa" name="no_hp_siswa" value="<?= set_value('no_hp_siswa') ?>" class=" form-control form-control-lg">
 							<?= form_error('no_hp_siswa', '<small class="text-danger">', '</small>') ?>
 						</div>
-						<div class="col-md-12 form-group">
-							<label for="username">Username</label>
-							<input type="text" id="username" name="username_siswa" value="<?= set_value('username_siswa') ?>" class=" form-control form-control-lg">
-							<?= form_error('username_siswa', '<small class="text-danger">', '</small>') ?>
+						<div class="col-md-6 form-group">
+							<label for="status">Status</label>
+							<select name="status" class="form-control" id="status">
+								<option>---PILIH STATUS---</option>
+								<option value="1">OrangTua</option>
+								<option value="2">Calon Siswa</option>
+							</select>
+							<!-- <input type="text" id="status" name="status" value="<?= set_value('status') ?>" class=" form-control form-control-lg"> -->
+							<?= form_error('status', '<small class="text-danger">', '</small>') ?>
 						</div>
-						<div class="col-md-12 form-group">
+					</div>
+					<div class="row">
+						<div class="col-md-6 form-group">
 							<label for="email">Email</label>
 							<input type="email" id="email" name="email" value="<?= set_value('email') ?>" class=" form-control form-control-lg">
 							<?= form_error('email', '<small class="text-danger">', '</small>') ?>
 						</div>
-						<div class="col-md-12 form-group">
+						<div class="col-md-6 form-group">
 							<label for="pword">Password</label>
 							<input type="password" id="pword" name="password_siswa" value="<?= set_value('password_siswa') ?>" class=" form-control form-control-lg">
 							<?= form_error('password_siswa', '<small class="text-danger">', '</small>') ?>
 						</div>
-						<div class="col-md-12 form-group">
+					</div>
+					<div class="row">
+						<div class="col-md-6 form-group">
 							<label for="pword2">Re-type Password</label>
 							<input type="password" id="pword2" name="ulangi_password_siswa" value="<?= set_value('ulangi_password_siswa') ?>" class="form-control form-control-lg">
 							<?= form_error('ulangi_password_siswa', '<small class="text-danger">', '</small>') ?>
-						</div>
-						<div class="col-md-12 form-group">
-							<label for="asal_sekolah">Asal Sekolah</label>
-							<input type="text" id="asal_sekolah" name="asal_sekolah" value="<?= set_value('asal_sekolah') ?>" class=" form-control form-control-lg">
-							<?= form_error('asal_sekolah', '<small class="text-danger">', '</small>') ?>
-						</div>
-						<div class="col-md-12 form-group">
-							<label for="alamat">alamat</label>
-							<input type="text" id="alamat" name="alamat" value="<?= set_value('alamat') ?>" class=" form-control form-control-lg">
-							<?= form_error('alamat', '<small class="text-danger">', '</small>') ?>
 						</div>
 					</div>
 					<div class="row">

c488a8f004981f0d21984244461f8eb186f27acf
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-09-04 10:51:18 +0700

    new detail persyratan di siswa

---
+++ application/views/frontend/pendaftaran/v_detail.php
@@ -0,0 +1,46 @@
+<div class="site-section ftco-subscribe-1 site-blocks-cover pb-4" style="background-image: url('<?= base_url('frontend/') ?>images/bg_1.jpg')">
+	<div class="container">
+		<div class="row align-items-end">
+			<div class="col-lg-7">
+				<h2 class="mb-0">Persyaratan Pendaftaran</h2>
+				<!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p> -->
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="custom-breadcrumns border-bottom">
+	<div class="container">
+		<a href="<?= base_url() ?>">Home</a>
+		<span class="mx-3 icon-keyboard_arrow_right"></span>
+		<span class="current">Persyaratan</span>
+	</div>
+</div>
+
+<div class="site-section">
+	<div class="container">
+		<div class="row mb-5">
+			<div class="col-lg-12 mb-lg-0 mb-4">
+				<table class="table table-striped table-bordered zero-configuration">
+					<thead>
+						<tr>
+							<th scope="col">Gambar</th>
+							<th scope="col">Keterangan</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($detail as $key => $value) { ?>
+							<tr>
+								<th scope="row"><img src="<?= base_url('assets/syarat/' . $value->gambar)  ?>" width="150px" alt=""></th>
+								<td><?= $value->keterangan ?></td>
+							</tr>
+
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+	</div>
+</div>
